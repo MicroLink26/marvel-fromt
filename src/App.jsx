41a9c1af692936e42a419fbe2c122499a82368c5
Mike <mic.roche@gmail.com>
2023-08-02 12:54:19 +0200

    dev environement added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 library.add(faStar);
+
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
